8d210d64afe1bc901e3586e40f11729daadf4c18
yvan-allioux <yvan.allioux.pro@gmail.com>
2021-05-13 13:04:41 +0200

    initial commit v2
    initial commit v2

---
+++ projet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int mdp = 0; // On initialise la variable à 0
+  
+  printf("mot de pass ");
+  scanf("%d", &mdp); // On demande d'entrer l'âge avec scanf
+  if(mdp==1234){
+    printf("ok");
+  }else{
+  printf("pas ok");
+  }
+  scanf("%d", &mdp);
+       
+  return 0;
+}

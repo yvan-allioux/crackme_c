29be5fd93d0d1a02c3e51deaed85d2b34509eb13
yvan-allioux <yvan.allioux.pro@gmail.com>
2021-11-23 16:18:47 +0100

    ptite modif de caractère

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
   int mdp = 0; // On initialise la variable à 0.
   
-  printf("mot de pass ");
+  printf("MDP");
   scanf("%d", &mdp); // On demande d'entrer l'âge avec scanf
   if(mdp==7){
-    printf("ok");
+    printf("Y");
   }else{
-  printf("pas ok");
+  printf("N");
   }
   //scanf("%d", &mdp);
        
